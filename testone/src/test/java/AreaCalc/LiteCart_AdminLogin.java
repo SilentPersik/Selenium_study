43a38ecd90f9d44674d19ddebe4f301283e49457
SilentPersik <rko.101290@gmail.com>
2021-11-13 20:10:04 +0600

    Немного доработал авторизацию в административном блоке сайта
    Также добавил признак Remember Me

--- testone/src/test/java/AreaCalc/LiteCart_AdminLogin.java
+++ testone/src/test/java/AreaCalc/LiteCart_AdminLogin.java
@@ -1,23 +1,30 @@
 package AreaCalc;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class LiteCart_AdminLogin {
+
     @Test
     public void LiteCart_AdminLogin() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost/litecart/admin/");
-        WebElement login = driver.findElement(By.name("username"));
-        login.sendKeys("Admin");
-        login.submit();
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("admin");
         WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("Admin");
-        password.submit();
+        password.sendKeys("admin");
+        WebElement checkbox = driver.findElement(By.name("remember_me"));
+        checkbox.click();
+        WebElement login = driver.findElement(By.name("login"));
+        login.click();
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.get("http://localhost/litecart/admin/");
         driver.quit();
-
     }
 }
